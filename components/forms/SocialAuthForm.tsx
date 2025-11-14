a1c6849ba03a207a4795cf02271d6ea175508789
BartoszStachowski <stachowskibartosz96@gmail.com>
2025-11-14 19:32:01 +0100

    sign-in and sign-up layout

---
+++ components/forms/SocialAuthForm.tsx
@@ -0,0 +1,34 @@
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+
+const SocialAuthForm = () => {
+  const buttonClass =
+    'flex-1 px-4 py-3.5 rounded-lg min-h-12 text-dark200_light800 background-dark400_light900 body-medium';
+  return (
+    <div className="flex flex-wrap gap-2.5 mt-10">
+      <Button className={buttonClass}>
+        <Image
+          src="/icons/github.svg"
+          alt="Github Logo"
+          width={20}
+          height={20}
+          className="invert-colors mr-2.5 object-contain"
+        />
+        <span>Log in with GitHub</span>
+      </Button>
+
+      <Button className={buttonClass}>
+        <Image
+          src="/icons/google.svg"
+          alt="Google Logo"
+          width={20}
+          height={20}
+          className="mr-2.5 object-contain"
+        />
+        <span>Log in with Google</span>
+      </Button>
+    </div>
+  );
+};
+
+export default SocialAuthForm;
