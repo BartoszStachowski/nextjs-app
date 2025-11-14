fa7d3fed95da8831867b2af86b211a91a1c33726
BartoszStachowski <stachowskibartosz96@gmail.com>
2025-11-14 15:20:14 +0100

    add shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 const page = () => {
-  
   return (
     <>
       <h1 className="h1-bold">Welcome no Inter</h1>
       <h1 className="font-inter h1-bold">Welcome here Inter</h1>
-      <h1 className="font-space-grotesk h1-bold">Welcome here SpaceGroteskVF</h1>
+      <h1 className="font-space-grotesk h1-bold">
+        Welcome here SpaceGroteskVF
+      </h1>
     </>
   );
 };
