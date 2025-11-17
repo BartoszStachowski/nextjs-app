242136d1ef61580929ffce66fc6eb25e9a786e11
BartoszStachowski <stachowskibartosz96@gmail.com>
2025-11-17 11:23:55 +0100

    remove key

--- components/navigation/navbar/NavLinks.tsx
+++ components/navigation/navbar/NavLinks.tsx
@@ -27,7 +27,6 @@ const NavLinks = ({ isMobileNav = false }: { isMobileNav?: boolean }) => {
         const LinkComponent = (
           <Link
             href={item.route}
-            key={item.label}
             className={cn(
               isActive
                 ? 'primary-gradient rounded-lg text-light-900'
